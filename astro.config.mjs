33e119dfa85edd1c82692f2313a59152493ffb33
Alan Aguirre <alan.aaguirrej@gmail.com>
2025-03-13 16:23:17 -0600

    Fix Base Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://alanaaguirre.github.io",
+  base: "/",
   output: "static", // Asegurar que Astro genera archivos estáticos
 });
